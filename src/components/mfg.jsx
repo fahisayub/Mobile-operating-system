0da0e8e4daa9a63ba6212d9296c1d8c0ff5b38b5
MUHAMMED FAHIZ P A <fahisayub@gmail.com>
2022-05-16 20:26:45 +0530

    first commit

---
+++ src/components/mfg.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import '../styles/mobile.css';
+
+let Manufacturer=()=>{
+    return <div className="box">
+        <h1>Mobile Manufacturers</h1>
+        <ul>
+            <li>Micromax</li>
+            <li>Apple</li>
+            <li>Samsung</li>
+            <li>HTC</li>
+        </ul>
+    </div>
+}
+export default Manufacturer;
---
+++ src/components/mobops.jsx
@@ -0,0 +1,18 @@
+import React from "react"
+import '../styles/mobile.css'
+const Operatingsys=()=>{
+return ( 
+    <div className="box">
+        <h1>Mobile Operating System</h1>
+<ul>
+    <li>Android</li>
+    <li>Blackberry</li>
+    <li>iPhone</li>
+    <li>Windows Phone</li>
+</ul>
+    </div>
+
+);
+
+}
+export default Operatingsys;
